27c0277e9b301a2f225a6eb598ab351540cec4cf
Ture Hassler <ture.hassler@gmail.com>
2023-09-01 13:58:55 +0200

    Task 2 done

--- A1/A1.py
+++ A1/A1.py
@@ -32,10 +32,37 @@ data.sort_values(by=['species'], inplace=True)
 
 # Questions part 1
 # What are the average length of sepals (sl) and their standard deviation?
-print(data.describe())
+print()
 print("Average length of sepals(sl): ", data["sl"].mean())
 print("Standard deviation of sepals(sl): ", data["sl"].std())
 print("Number of instances for each class: ", data.groupby('species').count())
+print()
+
+# TASK 2
+
+# 2.1
+# Remove -9999 values from dataset
+data = data[data.sl != -9999]
+print("Average sepal length after missing values removed: ", data["sl"].mean())
+print("Std sepal length after missing values removed: ", data["sl"].std())
+
+# 2.2
+# Plot using seaborn
+#sns.pairplot(data, hue="species")
+#plt.show()
+
+
+# Remove remaining outliers
+data = data[data.sw < 10]
+data = data[data.sl < 20]
+
+# Plot using seaborn
+#sns.pairplot(data, hue="species")
+#plt.show()
+
+# Questions part 2
+print("Average sepal length after outliers removed: ", data["sl"].mean())
+print("Std sepal length after outliers removed: ", data["sl"].std())
 
 
 
